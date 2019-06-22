60b27afb0ebe83afe862f3738db5925e13631604
Cristian <cristian.caballero@utec.edu.pe>
2019-06-21 20:25:15 -0500

    PC4 terminada, con TDD incluido

--- PC4/main.cpp
+++ PC4/main.cpp
@@ -1,53 +1,70 @@
 #include <iostream>
 #include <vector>
-#include "Matriz.h"
+#include "Concurrente.h"
 #include <ctime>
+#include <thread>
 
 using namespace std;
 
 
 int main() {
-    Matriz<int> matriz1 =Matriz<int>(2,3);
-    matriz1.AnadirElemento(1);
-    matriz1.AnadirElemento(2);
-    matriz1.AnadirElemento(3);
-    matriz1.AnadirElemento(4);
-    matriz1.AnadirElemento(1);
-    matriz1.AnadirElemento(9);
-    matriz1.imprimirMatriz();
+    Matriz<int> m1 =Matriz<int>(2,3);
+    for(int i=0;i<m1.filas;i++){
+        for(int j=0;j<m1.columnas;j++){
+            m1.AnadirElemento(2,i,j);
+        }
+    }
+    m1.imprimirMatriz();
     cout<<endl;
 
 
-    Matriz<int> matriz2 = Matriz<int>(3,4);
-    matriz2.AnadirElemento(5);
-    matriz2.AnadirElemento(5);
-    matriz2.AnadirElemento(5);
-    matriz2.AnadirElemento(5);
-    matriz2.AnadirElemento(5);
-    matriz2.AnadirElemento(5);
-    matriz2.AnadirElemento(5);
-    matriz2.AnadirElemento(5);
-    matriz2.AnadirElemento(5);
-    matriz2.AnadirElemento(5);
-    matriz2.AnadirElemento(5);
-    matriz2.AnadirElemento(5);
+    Matriz<int> m2 = Matriz<int>(3,4);
+    for(int i=0;i<m2.filas;i++){
+        for(int j=0;j<m2.columnas;j++){
+            m2.AnadirElemento(3,i,j);
+        }
+    }
 
-    matriz2.imprimirMatriz();
+   m2.imprimirMatriz();
     cout<<endl;
 
 
     unsigned t0, t1;
     try {
         t0=clock();
-        Matriz<int> matriz3 = matriz1 * matriz2;
+        Matriz<int> m3 = m1 ^ m2;
         t1=clock();
         double time = (double(t1-t0)/CLOCKS_PER_SEC);
         cout<< "Tiempo de ejecución:"<<time<<endl;
-        matriz3.imprimirMatriz();
+        m3.imprimirMatriz();
     }
     catch (exception& e)
     {
         cout<<e.what();
     }
+    cout<<endl;
 
+    /*Matriz<int> m4 =Matriz<int>(3,4);
+    for(int i=0;i<3;i++){
+        for(int j=0;j<4;j++){
+            m4.AnadirElemento(5,i,j);
+        }
+    }
+    m4.imprimirMatriz();
+    cout<<endl;
+
+
+    Matriz<int> m5 = Matriz<int>(4,4);
+    for(int i=0;i<4;i++){
+        for(int j=0;j<4;j++){
+            m5.AnadirElemento(1,i,j);
+        }
+    }
+
+    m5.imprimirMatriz();
+    cout<<endl;*/
+
+    Matriz<int>m6=m1*m2;
+    m6.imprimirMatriz();
+    //thread::hardware_concurrency();
 }
---
+++ PC4/test1.cpp
@@ -0,0 +1,37 @@
+//
+// Created by cristian on 6/21/19.
+//
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include "Concurrente.h"
+SCENARIO("Cálculo de la diagonal de la matriz que resulta de la multiplicación no concurrente de 2 matrices")
+{
+    GIVEN("Matriz 1 - 3x4 y Matriz 2 - 4x5"
+    )
+    {
+        WHEN("Matriz 1 rellena de 2's y Matriz 2 rellena de 3's")
+        {
+            Matriz<int> m1=Matriz<int>(3,4);
+            for(int i=0;i<3;i++){
+                for(int j=0;j<4;j++){
+                    m1.AnadirElemento(2,i,j);
+                    }
+                }
+            Matriz<int> m2=Matriz<int>(4,5);
+            for(int i=0;i<4;i++){
+                for(int j=0;j<5;j++){
+                    m2.AnadirElemento(3,i,j);
+                    }
+                }
+
+                Matriz<int> m3 = m1 ^ m2;
+
+            auto Total = m3.diagonal();
+            THEN("La diagonal es 72")
+            {
+                REQUIRE(Total==72);
+            }
+        }
+    }
+}
+
